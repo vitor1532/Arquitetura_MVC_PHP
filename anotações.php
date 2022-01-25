0fac2703fcb1d4d1e9f35c057f071c793a19e900
unknown <vitomartinsalmeida@gmail.com>
2022-01-25 14:58:37 -0300

    anotações iniciais

---
+++ anotações.php
@@ -0,0 +1,81 @@
+<?php
+
+//Primeira Aula
+
+/*
+Padrão de arquitetura (Architectural Pattern)
+
+Cliente-Servidor
+P2P
+Quadro Negro
+Criação implícita
+Pipes e filtros
+Plugin
+Análise de sistema estruturada
+Arquitetura orientada a serviço
+Arquitetura orientada a busca
+MVC
+Computação distribuída
+Aplicação monolítica
+Modelo em três camadas
+*/
+
+/*
+Padrão de projeto (Design Pattern)
+
+by Gof (Gang of Four):
+
+PADRÕES DE CRIAÇÃO:
+
+Abstract Factory
+Builder
+Factory Method
+Prototype
+Singleton
+//------------------//
+
+PADRÕES ESTRUTURAIS:
+
+Adapter
+Facade
+Bridge
+Decorator
+FlyWeight
+Composite
+Proxy
+
+//-----------------//
+
+PADRÕES COMPORTAMENTAIS:
+
+Chain of Responsability
+Command
+Interpreter
+Iterator
+Mediator
+Memento
+Observer
+State
+Strategy
+Template Method
+Visitor
+
+//---------------------------------------------//
+
+by GRASP (General Responsability Assignment Software Patterns (or Principles))
+
+Controller
+Creator
+Indirection
+Information Expert
+Hight Cohesion
+Loose coupling
+Polymorphism
+Protected variations
+Pure fabrication
+
+*/
+
+
+
+?>
