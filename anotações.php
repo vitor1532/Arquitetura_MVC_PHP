22a5fd481dfa3e3cae48975743bc7b88d840e277
unknown <vitomartinsalmeida@gmail.com>
2022-01-25 15:56:14 -0300

    final da primeira aula

--- anotações.php
+++ anotações.php
@@ -5,6 +5,7 @@
 /*
 Padrão de arquitetura (Architectural Pattern)
 
+
 Cliente-Servidor
 P2P
 Quadro Negro
@@ -22,6 +23,7 @@ Modelo em três camadas
 
 /*
 Padrão de projeto (Design Pattern)
+
 
 by Gof (Gang of Four):
 
@@ -73,6 +75,17 @@ Loose coupling
 Polymorphism
 Protected variations
 Pure fabrication
+
+
+//---------------------------//
+
+//Architectural Pattern são Requisitos funcionais. Recursos da aplicação em si, ou seja, ligados diretamente as funcionalidades da aplicação
+
+//Design Patterns são requisitos não funcionais. Recursos que não estão diretamente relacionados com as funcionalidades da aplicação
+
+A partir dos requisitos funcionais, é possível identificar diversos design patterns que poderão ser utilizados para resolver problemas relativos às funcionalidades da aplicação em si
+
+
 
 */
 
